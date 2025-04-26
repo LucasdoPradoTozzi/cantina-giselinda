cda00fa163e2bd1a029d240f9b5e95c6cf6cf4de
LucasdoPradoTozzi <lucasdopradotozzidois@gmail.com>
2025-04-26 11:51:49 -0300

    update: created button to submit the sell, changed the layout to addapt to errors and show each product with better ux

--- app/Livewire/CreateSell.php
+++ app/Livewire/CreateSell.php
@@ -7,6 +7,11 @@ use Livewire\Component;
 use App\Services\MoneyService;
 
 use App\Models\Product;
+use App\Models\Sell;
+use App\Models\SoldItem;
+use App\Models\Stock;
+use Exception;
+use Illuminate\Support\Facades\DB;
 
 class CreateSell extends Component
 {
@@ -114,6 +119,87 @@ class CreateSell extends Component
 
         $this->selectedProductId = null;
         $this->selectedQuantity = 1;
+    }
+
+    public function submitSell()
+    {
+        if (empty($this->items)) {
+            $this->addError('purchase', 'Seu carrinho está vazio.');
+            return;
+        }
+
+        $this->validate([
+            'items' => 'required|array|min:1',
+            'items.*.product_id' => [
+                'required',
+                'integer',
+                'exists:products,id',
+            ],
+            'items.*.amount' => [
+                'required',
+                'integer',
+                'min:1',
+            ],
+        ], [
+            'items.*.product_id.required' => 'Produto inválido no carrinho.',
+            'items.*.product_id.exists' => 'Produto selecionado não existe.',
+            'items.*.amount.required' => 'Quantidade obrigatória para todos os produtos.',
+            'items.*.amount.min' => 'Quantidade deve ser no mínimo 1.',
+        ]);
+
+
+        try {
+
+
+            DB::beginTransaction();
+
+            $sell = Sell::create([
+                'title' => now()->format('d/m/Y H:i')
+            ]);
+
+            $SellId = $sell->id;
+
+            $moneyService = new MoneyService();
+
+            foreach ($this->items as $product) {
+                $productById = Product::where('id', $product['product_id'])->first();
+                if (!$productById) throw new Exception('Produto não encontrado, verifique se o mesmo ainda existe.');
+
+                $wasAOffer = 0;
+
+                $soldPrice = $moneyService->getMultiplicationIntegerValue($productById->value, $product['amount']);
+
+                SoldItem::create([
+                    'product_id' => $product['product_id'],
+                    'sell_id' => $SellId,
+                    'amount' => $product['amount'],
+                    'price_by_item' => $productById->value,
+                    'sold_price' => $soldPrice,
+                    'was_a_offer' => $wasAOffer
+                ]);
+
+                $stock = Stock::where('product_id', $productById->id)->first();
+
+                if (!$stock) throw new Exception('Erro ao buscar estoque do produto: ' . $productById->name);
+
+                if (((int) $stock->quantity - (int) $product['amount']) < 0)
+                    throw new Exception($productById->name .
+                        ' não tem estoque suficiente para essa venda. Quantidade solicitada: ' .
+                        $product['amount'] .
+                        ' quantidade disponível: ' .
+                        $stock->quantity);
+
+                $stock->quantity -= $product['amount'];
+                $stock->save();
+            }
+
+            DB::commit();
+            return redirect('/sells');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            $this->addError('purchase', $e->getMessage());
+            return;
+        }
     }
 
     public function render()
