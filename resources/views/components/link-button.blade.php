ad7c629ebf8d30e5c9ec868c325a7913d6d71c9f
LucasdoPradoTozzi <lucasdopradotozzidois@gmail.com>
2025-05-05 19:02:15 -0300

    layout: revert button changes

--- resources/views/components/link-button.blade.php
+++ resources/views/components/link-button.blade.php
@@ -1,3 +1,3 @@
-<a {{ $attributes->merge(['class' => 'bg-gray-800 hover:bg-gray-600 rounded h-10 px-6 font-bold flex items-center justify-center']) }}>
+<a {{ $attributes(['class' => 'bg-gray-800 hover:bg-gray-600 rounded py-2 px-6 font-bold']) }}>
     {{ $slot }}
 </a>
